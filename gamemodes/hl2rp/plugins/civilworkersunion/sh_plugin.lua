a4659ce3cbbdcff6d67f18e1df7220a38a612f9e
NDDev <NDDev@users.noreply.github.com>
2014-12-06 18:52:16 -0500

    fix faction err

--- gamemodes/hl2rp/plugins/civilworkersunion/sh_plugin.lua
+++ gamemodes/hl2rp/plugins/civilworkersunion/sh_plugin.lua
@@ -1,6 +1,7 @@
 function PLUGIN:GetDefaultInv(inventory, client, data)
 if (data.faction == FACTION_CIVILWORKER) then
 data.chardata.digits = nut.util.GetRandomNum(5)
+end
 
 inventory:Add("cid2", 1, {
 Name = data.charname,
