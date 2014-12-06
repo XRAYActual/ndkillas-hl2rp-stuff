4d8723e60a4471d6bc2e65f3df2a6d13005584bc
NDDev <NDDev@users.noreply.github.com>
2014-12-06 18:09:38 -0500

    plugins update #1

---
+++ lua/Small-Scripts/lua/autorun/server/workshop.lua
@@ -0,0 +1,7 @@
+--Bare bones just to show the command.
+resource.AddWorkshop("") --Add the workshop ID in the quotes. http://i.imgur.com/luTn5xt.png The numbers at the end of the link.
+
+--Example
+resource.AddWorkshop("171962560") -- rp_industrial17_v1 map
+resource.AddWorkshop("128089118") -- m9k
+resource.AddWorkshop("128093075") -- m9k
