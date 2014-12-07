9517a1eee9179520accfe5fd785833c24225b77d
Bubbha1 <brandonlucas7777@gmail.com>
2014-12-06 21:19:24 -0800

    Rebel job changes
    i luv u jim

---
+++ gamemodes/hl2rp/gamemode/schema/classes/sh_rebel.lua
@@ -0,0 +1,9 @@
+CLASS.name = "Rebel"
+CLASS.faction = FACTION_REBEL
+CLASS.model = Model("models/player/eli.mdl")
+
+function CLASS:PlayerCanJoin(client)
+	
+end
+
+CLASS_Rebal = CLASS.index
---
+++ gamemodes/hl2rp/gamemode/schema/factions/sh_rebel.lua
@@ -0,0 +1,5 @@
+FACTION.name = "Rebel"
+FACTION.desc = "The resistance of the city,"
+FACTION.color = Color(86, 56, 56)
+
+FACTION_REBEL = FACTION.index
